91b93490d163fc715074f4bccf37926a916cd150
Kellie Henderson <kellie.henderson@my.tccd.edu>
2019-11-06 07:55:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+//Kellie Henderson, COSC 1436-57005, repl.it
+//This is a program to help Joe figure out what  monetary donimonations he will get from the bank.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+//Declare and initialize the variable for Joe's Pennies.
+int pennies = 0;
+
+while (pennies != 0)
+{
+  cout << "\nPlease enter how many pennies you have: " << endl;
+  cin >> pennies;
+}
+if (pennies 
+
+
+return 0;
+
+}
